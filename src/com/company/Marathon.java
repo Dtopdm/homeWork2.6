0f359253d25af696b31ce6cbe09d1baa74196f0c
dtopdm <dtop.dm@gmail.com>
2020-10-01 03:41:41 +0600

    Initial commit

---
+++ src/com/company/Marathon.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Marathon<T extends Number, S extends Number> implements Runnable<T,S> {
+    private T distance;
+    private S speed;
+
+    public Marathon(T distance, S speed) {
+        this.distance = distance;
+        this.speed = speed;
+    }
+
+    public T getDistance() {
+        return distance;
+    }
+
+    public S getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void runner(T distance, S speed) {
+
+    }
+}
---
+++ src/com/company/Runnable.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface Runnable<T,S> {
+   public void runner(T distance, S speed);
+}
